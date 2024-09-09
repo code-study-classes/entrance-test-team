8d66d84e36b81bdaa2b4989cf683f7cda51be55a
kalininaekaterina2004 <119296172+kalininaekaterina2004@users.noreply.github.com>
2024-09-09 08:16:07 +0000

    hotels

---
+++ hotels.js
@@ -0,0 +1,10 @@
+import regions from "./region";
+const hotels = [
+    {id: 1,
+    name: "Ясное солнышко",
+    region: regions.indexOf(1)},
+    {id: 2,
+        name: "Солнышко ясное",
+        region: regions.indexOf(0)}
+]
+export default hotels;
